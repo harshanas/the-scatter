39c819f0ffbb5a0b74efca607c84997642ceffba
Harshana Serasinghe <harshanadabezt@gmail.com>
2022-05-28 19:15:35 +0530

    Implement CRU operations in contract

--- contracts/TheScatter.sol
+++ contracts/TheScatter.sol
@@ -2,21 +2,87 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TheScatter {
-    string private greeting;
+    address public owner;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    using Counters for Counters.Counter;
+    Counters.Counter private _storyIds;
+
+    constructor() {
+        console.log("Deploying The Scatter Contract");
+        owner = msg.sender;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    struct Story {
+      uint id;
+      string title;
+      string content;
+      bool isPublished;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    mapping(uint => Story) private idToStory;
+    mapping(string => Story) private hashToStory;
+    mapping(uint => address) private storyIdToWalletAddr;
+    mapping(address => Story[]) private walletAddrToStory;
+
+    event PostCreated(uint id, string title, string hash);
+    event PostUpdated(uint id, string title, string hash, bool published);
+
+    function createStory(string memory title, string memory hash) public {
+        _storyIds.increment();
+        uint storyId = _storyIds.current();
+
+        Story storage story = idToStory[storyId];
+        story.id = storyId;
+        story.title = title;
+        story.isPublished = true;
+        story.content = hash;
+
+        hashToStory[hash] = story;
+        
+        storyIdToWalletAddr[storyId] = msg.sender;
+        uint currentStoryCountByAuthor = walletAddrToStory[msg.sender].length;
+        walletAddrToStory[msg.sender][currentStoryCountByAuthor+1] = story;
+
+        emit PostCreated(storyId, title, hash);
     }
+
+    function updateStory(uint storyId, string memory title, string memory hash, bool isPublished) public  {
+        address authorId = storyIdToWalletAddr[storyId];
+        require(msg.sender == authorId, "Only the author can edit their story");
+
+        Story storage story =  idToStory[storyId];
+        story.title = title;
+        story.isPublished = isPublished;
+        story.content = hash;
+
+        hashToStory[hash] = story;
+
+        emit PostUpdated(story.id, title, hash, isPublished);
+    }
+
+    function fetchStories(address authorId) public view returns (Story[] memory) {
+        uint storyCountByAuthor = walletAddrToStory[authorId].length;
+
+        Story[] memory stories = new Story[](storyCountByAuthor);
+        for (uint currentStoryId = 0; currentStoryId < storyCountByAuthor; currentStoryId++) {
+
+            Story storage currentItem = walletAddrToStory[authorId][currentStoryId];
+            stories[currentStoryId] = currentItem;
+        }
+        return stories;
+    }
+
+    function fetchStory(address walletAddr, uint storyId) public view returns (Story memory){
+        require(storyIdToWalletAddr[storyId] == walletAddr, "Cannot find the story");
+        return idToStory[storyId];
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner);
+    _;
+    }
+
 }
