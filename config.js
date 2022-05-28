fa4fcc037323587cc5bed588be1e51c9f03f6d99
Harshana Serasinghe <harshanadabezt@gmail.com>
2022-05-28 09:43:32 +0530

    Deleted Old MVP

--- config.js
+++
@@ -1,4 +0,0 @@
-
-  export const contractAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  
--- pages/admin/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextFetchEvent, NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    return NextResponse.next();
-}
