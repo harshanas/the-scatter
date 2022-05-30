a5697aa3b03312978423f0ceb77ea6ca6737a480
Harshana Serasinghe <harshanadabezt@gmail.com>
2022-05-30 16:18:16 +0530

    Add stats to home page

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,20 @@ import react, { useContext } from 'react';
 import Link from 'next/link'
 import Head from 'next/head';
 import Image from 'next/image';
+import { ethers } from 'ethers'
 
 import AccountConnection from '../components/AccountConnection'
 import MainLayout from '../layouts/mainLayout'
+import { contractAddress } from '../config';
 import { AccountContext }  from "../lib/context";
+import { getRpcProvider } from "../lib/common"
+
+import Scatter from "../artifacts/contracts/TheScatter.sol/TheScatter.json"
 
 import styles from '../assets/styles/Home.module.css'
 import ScatterLogo from "../public/images/logo_home.png"
 
-export default function Home() {
+export default function Home({ stats }) {
   const { walletAddr } = useContext(AccountContext);
 
   return (
@@ -52,6 +57,24 @@ export default function Home() {
               
             </p>
           </main>
+          
+          <div className="row mt-5">
+            <h4>Stats</h4>
+            <hr/>
+            <div className="col">
+              <div>
+                <h4 className="fw-bold mb-0">{ stats.authorsCount} </h4>
+                <p>Authors</p>
+              </div>
+            </div>
+
+            <div class="col">
+              <div>
+                <h4 className="fw-bold mb-0">{ stats.storiesCount}</h4>
+                <p>Stories</p>
+              </div>
+            </div>
+          </div>
 
           <footer className="mt-auto text-white-50">
             
@@ -69,4 +92,23 @@ Home.getLayout = function getLayout(page) {
       {page}
     </MainLayout>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  let provider = getRpcProvider();
+  let stats = {}
+
+  const contract = new ethers.Contract(contractAddress, Scatter.abi, provider)
+  const authorsCount = await contract.authorsCount();
+  const storiesCount = await contract.storiesCount();
+
+  return {
+    props: {
+      stats:{
+        authorsCount: parseInt(authorsCount._hex, 16),
+        storiesCount: parseInt(storiesCount._hex, 16)
+      }
+    }
+  }
 }
